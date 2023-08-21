c6bebe2a31fb52b66d091a03a06c88100a69491c
ESPR3SS0 <98864173+ESPR3SS0@users.noreply.github.com>
2023-08-21 12:15:52 -0400

    Script to list all keybinds

---
+++ README.md
@@ -0,0 +1,7 @@
+Zelkeys  !! 
+-----------
+
+I like the compact zellij layout... but I forget the keys. 
+
+This project will list the keys and in the future allow searching 
+of actions! 
---
+++ first.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+def load_keybind_section(config: Path = Path("~/.config/zellij/config.kdl").expanduser()):
+    '''
+    Load the keybinds from the config file
+    '''
+
+
+    # Flag for whether or we are in the keybind potion of the file
+    in_binds = False
+
+    # This will be a list of the stripped lines
+    bind_section = []
+
+    with open(config, "r") as f:
+
+        # Iterate over the lines
+        for line in f.readlines():
+
+            # If were not current in the keybinds, see if this 
+            # is the first line of the keybinds definition
+            if not in_binds:
+                if "keybinds" in line:
+                    in_binds = True
+                else:
+                    continue
+
+            # If we get here we are in the keybind section
+            bind_section.append(line.strip())
+
+    return bind_section
+
+def parse_keybinds(rawlines: list[str]):
+    '''
+    Parse the keybinds from load_keybind_section
+    '''
+
+
+    clean_lines = []
+    cur_section_name = "None"
+
+    for line in rawlines:
+        if "bind" not in line and "{" in line:
+            cur_section_name = line.split()[0]
+        # Start by getting every line that has bind in it
+        if "bind" in line:
+            formated_line = line.replace("{", "is")
+            formated_line = formated_line.replace("}", "")
+            formated_line = formated_line.replace(";", "")
+
+            clean_lines.append((cur_section_name,formated_line))
+
+    return clean_lines
+
+def pretty_print_keys(keys: list[str]):
+    for key in keys:
+        print(f"{key[0]:<4} | {key[1]}")
+    return
+
+
+if __name__ == "__main__":
+    pretty_print_keys(parse_keybinds(load_keybind_section()))
